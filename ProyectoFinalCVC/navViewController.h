b42e4fb74536979045a109af4508538b2203d963
andres25 <alumno@ESL-3082.local>
2015-04-16 12:30:31 -0500

    Commit Inicial

---
+++ ProyectoFinalCVC/navViewController.h
@@ -0,0 +1,13 @@
+//
+//  navViewController.h
+//  ProyectoFinalCVC
+//
+//  Created by alumno on 4/16/15.
+//  Copyright (c) 2015 ITESM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface navViewController : UINavigationController
+
+@end
---
+++ ProyectoFinalCVC/navViewController.m
@@ -0,0 +1,37 @@
+//
+//  navViewController.m
+//  ProyectoFinalCVC
+//
+//  Created by alumno on 4/16/15.
+//  Copyright (c) 2015 ITESM. All rights reserved.
+//
+
+#import "navViewController.h"
+
+@interface navViewController ()
+
+@end
+
+@implementation navViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
